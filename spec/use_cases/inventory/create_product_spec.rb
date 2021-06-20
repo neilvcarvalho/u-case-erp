e0c80f9e209c6a061912a5dd047e79b7b669d3cd
Neil <me@neil.pro>
2021-06-20 16:28:15 +0000

    test: add FactoryBot for test factories

--- spec/use_cases/inventory/create_product_spec.rb
+++ spec/use_cases/inventory/create_product_spec.rb
@@ -1,27 +1,24 @@
 require "rails_helper"
 
 describe Inventory::CreateProduct do
-  context "with valid params" do
-    it "returns a success state" do
-      company = Company.create!(email: "me@neil.pro", password: "foobar123")
-      params = ActionController::Parameters.new(product: { sku: "1", name: "Product" })
+  let(:company) { FactoryBot.create(:company) }
 
+  context "with valid params" do
+    let(:params) { ActionController::Parameters.new(product: { sku: "1", name: "Product" }) }
+
+    it "returns a success state" do
       creation = described_class.call(company: company, params: params)
 
       expect(creation).to be_success
     end
 
     it "creates a product for the provided company" do
-      company = Company.create!(email: "me@neil.pro", password: "foobar123")
-      params = ActionController::Parameters.new(product: { sku: "1", name: "Product" })
-
-      expect { described_class.call(company: company, params: params) }.to change(company.inventory_products, :count).by(1)
+      expect {
+        described_class.call(company: company, params: params)
+      }.to change(company.inventory_products, :count).by(1)
     end
 
     it "returns the created product as the result" do
-      company = Company.create!(email: "me@neil.pro", password: "foobar123")
-      params = ActionController::Parameters.new(product: { sku: "1", name: "Product" })
-
       creation = described_class.call(company: company, params: params)
       product = creation.data[:product]
 
@@ -30,19 +27,15 @@ describe Inventory::CreateProduct do
   end
 
   context "without the `product` parameter" do
-    it "returns a failure as parameter_missing" do
-      company = Company.create!(email: "me@neil.pro", password: "foobar123")
-      params = ActionController::Parameters.new(sku: "1", name: "Product")
+    let(:params) { ActionController::Parameters.new(sku: "1", name: "Product") }
 
+    it "returns a failure as parameter_missing" do
       creation = described_class.call(company: company, params: params)
 
       expect(creation.type).to eq :parameter_missing
     end
 
     it "returns the error as the result" do
-      company = Company.create!(email: "me@neil.pro", password: "foobar123")
-      params = ActionController::Parameters.new(sku: "1", name: "Product")
-
       creation = described_class.call(company: company, params: params)
 
       expect(creation.data[:message]).to include "param is missing"
