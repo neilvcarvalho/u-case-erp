362bcbebffd15204a47d0ce6aeb02c11d3d001ae
Neil <me@neil.pro>
2021-06-21 11:54:34 +0000

    fix: specify a :dependent option for has_many relationships

--- app/models/inventory/product.rb
+++ app/models/inventory/product.rb
@@ -1,7 +1,7 @@
 class Inventory::Product < ApplicationRecord
   belongs_to :company, inverse_of: :inventory_products
   has_many :stock_movements, class_name: "Inventory::StockMovement", foreign_key: :inventory_product_id,
-                             inverse_of: :product
+                             inverse_of: :product, dependent: :destroy
 
   validates :reserved_quantity, numericality: { greater_than_or_equal_to: 0,
                                                 less_than_or_equal_to: ->(product) { product.quantity } }
